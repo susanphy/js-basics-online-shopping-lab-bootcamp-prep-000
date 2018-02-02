1ba5b3a342e13651ea5c56210b94fdb108fe261f
Susan <suzutoyuki@gmail.com>
2018-02-02 01:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
   for(let i =0; i<cart.length; i++){
            if (cart[i].hasOwnProperty(item)){
              cart.splice(i, 1)
-             return cart;
+          ;
 
   if(item || Object.keys(cart)){
      console.log("That item is not in your cart.")}
