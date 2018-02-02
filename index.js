2817248a28312bd4a04a85f9330b125eab1f5698
Susan <suzutoyuki@gmail.com>
2018-02-02 02:01:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
   for( var i=0; i< cart.length; i++) {
     if (Object.keys(cart[i])[0] == item ){
       console.log("in the loop-found");
-    
+
       total -= cart[i][item];
     cart.splice(i,1);
     return cart;
